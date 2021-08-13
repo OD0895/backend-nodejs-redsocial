426ee6d86823afb2399ea326457ddf32df22f0fd
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-08-13 08:26:14 -0500

    :memo: Documentacion de nuestr Api con swagger

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const config = require('../config');
+
+const swaggerUi = require('swagger-ui-express');
 const user = require('./components/user/network');
+const config = require('../config');
+
 
 const app = express();
 
 app.use(express.json());
 
+const swaggerDoc = require('./swagger.json');
+
 // ROUTER
+app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api/user', user);
 
 app.listen(config.api.port, () => {
