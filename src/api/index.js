7391105019cda7c4d00a2c2d2911882dbe0d1dab
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-08-15 00:47:25 -0500

    :wrench: Autenticacion basada en tokens

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
 
-const swaggerUi = require('swagger-ui-express');
-const user = require('./components/user/network');
-const config = require('../config');
+const swaggerUi = require("swagger-ui-express");
 
+const config = require("../config.js");
+const user = require("./components/user/network");
+const auth = require("./components/auth/network");
+const errors = require("../network/errors");
 
 const app = express();
 
 app.use(express.json());
 
-const swaggerDoc = require('./swagger.json');
+const swaggerDoc = require("./swagger.json");
+const { error } = require("../network/response.js");
 
-// ROUTER
-app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-app.use('/api/user', user);
+// ROUER
+app.use("/api/user", user);
+app.use("/api/auth", auth);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.use(errors);
 
 app.listen(config.api.port, () => {
-  console.log(`API listening on port ${config.api.port}`);
+  console.log("Api escuchando en el puerto ", config.api.port);
 });
