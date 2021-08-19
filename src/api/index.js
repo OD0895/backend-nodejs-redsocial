364ee6b05197240ce36c1e70afc1dff833c63b65
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-08-18 23:22:18 -0500

    :sparkles: Separando los posts a un microservicio

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const swaggerUi = require("swagger-ui-express");
 const config = require("../config.js");
 const user = require("./components/user/network");
 const auth = require("./components/auth/network");
-const post = require('./components/post/network');
+//const post = require('./components/post/network');
 const errors = require("../network/errors");
 
 const app = express();
@@ -18,7 +18,7 @@ const { error } = require("../network/response.js");
 // ROUER
 app.use("/api/user", user);
 app.use("/api/auth", auth);
-app.use("/api/post", post);
+//app.use("/api/post", post);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors);
--- src/config.js
+++ src/config.js
@@ -1,21 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   api: {
-      port: process.env.API_PORT || 3000,
+    port: process.env.API_PORT || 3000,
+  },
+  post: {
+    port: process.env.POST_PORT || 3002,
   },
   jwt: {
-      secret: process.env.JWT_SECRET || 'jwt-secret',
+    secret: process.env.JWT_SECRET || "jwt-secret",
   },
   mysql: {
-      host: process.env.MYSQL_HOST || 'localhost',
-      port: process.env.MYSQL_PORT || 3306,
-      user: process.env.MYSQL_USER || 'root',
-      password: process.env.MYSQL_PASSWORD || 'root',
-      database: process.env.MYSQL_DATABASE || 'db',
+    host: process.env.MYSQL_HOST || "localhost",
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "root",
+    database: process.env.MYSQL_DATABASE || "db",
   },
   mysqlService: {
-      host: process.env.MYSQL_SERVICE_HOST || 'localhost',
-      port: process.env.MYSQL_SERVICE_PORT || 3001,
-  }
-}
+    host: process.env.MYSQL_SERVICE_HOST || "localhost",
+    port: process.env.MYSQL_SERVICE_PORT || 3001,
+  },
+};
